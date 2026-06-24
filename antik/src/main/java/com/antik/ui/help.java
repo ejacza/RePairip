5f9c83347322b908efe352954aef9b3df15bcb69
isPointer <aantikmods@gmail.com>
2026-06-24 09:10:52 +0600

    Update help.java

--- antik/src/main/java/com/antik/ui/help.java
+++ antik/src/main/java/com/antik/ui/help.java
@@ -4,6 +4,7 @@ public class help {
 
     public static void help() {
         System.out.println("java -jar RePairip.jar -i <input.apks>");
+        System.out.println("java -jar RePairip.jar -i <input.apks> -r <lazymod>");
         System.out.println("java -jar RePairip.jar -i <merged.apk> -t <pairip.json>");
     }
 }
